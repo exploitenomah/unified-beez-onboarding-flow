1bc55bf152a68ec004971aac5f28d0e705d8f13c
exploitenomah <iksploiting@gmail.com>
2025-11-21 17:43:16 +0100

    feat: create newsTicker component

---
+++ app/components/newsTicker/InfiniteNewsTicker.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { InfiniteNewsTickerProps } from "./types";
+import { useAutoScrollLoop } from "./hooks";
+
+export const InfiniteNewsTicker = ({
+  items,
+  speed = 1,
+  gap = 32,
+  className,
+  pauseOnHover = true,
+}: InfiniteNewsTickerProps) => {
+  const { containerRef, isPausedRef } = useAutoScrollLoop({
+    speed,
+  });
+
+  return (
+    <div
+      className={`overflow-hidden w-full ${className}`}
+      onMouseEnter={() => pauseOnHover && (isPausedRef.current = true)}
+      onMouseLeave={() => pauseOnHover && (isPausedRef.current = false)}
+    >
+      <div
+        ref={containerRef}
+        className="inline-flex whitespace-nowrap"
+        style={{ gap: `${gap}px` }}
+      >
+        {[...items, ...items].map((item, idx) => (
+          <div key={idx} className="shrink-0">
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
